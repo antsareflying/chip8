aeacf2614412a84066815e20a7da84b5240d78d4
antsareflying <seongmumoon@gmail.com>
2021-09-06 12:41:17 +0800

    implemented some opcodes

--- chip8.c
+++ chip8.c
@@ -5,6 +5,11 @@
 
 #define SCREEN_WIDTH 64
 #define SCREEN_HEIGHT 32
+#define unknown_opcode(opcode)\
+		do { \
+			fprintf(stderr, "Unknown opcode: %x", opcode); \
+			exit(EXIT_FAILURE); \
+		} while (0)
 
 uint8_t memory[4096];
 uint8_t V[16];
@@ -38,7 +43,7 @@ const uint8_t fontset[80] =
 	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-int main(void)
+int main(int argc, char** argv)
 {
 	return 0;
 }
@@ -88,4 +93,118 @@ int chip8_load_program(char* program_name)
 	}
 
 	return 0;
+}
+
+void fetch_decode_execute(void)
+{
+	/*fetch opcode*/
+	opcode = memory[PC] << 8 | memory[PC + 1];
+	uint8_t X = (opcode >> 8) & 0x000F;
+	uint8_t Y = (opcode >> 4) & 0x000F;
+	
+	/*decode and execute opcode*/
+	switch(opcode & 0xF000)
+	{
+		case 0x0000:
+			break;
+		case 0x1000: /*1NNN jumps to address NNN*/
+			PC = opcode & 0xFFF;
+			break;
+		case 0x2000:
+			break;
+		case 0x3000: /*3XNN Skips the next instruction if VX equals NN*/
+			PC += (V[X] == (opcode & 0x00FF)) ? 4 : 2;
+			break;
+		case 0x4000: /*4XNN Skips the next instruction if VX doesn't equal NN*/
+			PC += (V[X] != (opcode & 0x00FF)) ? 4 : 2;
+			break;
+		case 0x5000: /*5XY0 Skips the next instruction if VX equals VY*/
+			switch(opcode & 0x000F)
+			{
+				case 0:
+					PC += (V[X] == V[Y]) ? 4 : 2;
+					break;
+				default:
+					unknown_opcode(opcode);
+			}
+			break;
+		case 0x6000: /*6XNN sets VX to NN*/
+			V[X] = opcode & 0x00FF;
+			PC += 2;
+			break;
+		case 0x7000: /* 7XNN Adds NN to VX*/
+			V[X] += opcode & 0x00FF;
+			PC += 2;
+			break;
+		case 0x8000:
+			switch(opcode & 0x000F)
+			{
+				case 0x0000:
+					V[X] = V[Y];
+					break;
+				case 0x0001:
+					V[X] = V[X] | V[Y];
+					break;
+				case 0x0002:
+					V[X] = V[X] & V[Y];
+					break;
+				case 0x0003:
+					V[X] = V[X] ^ V[Y];
+					break;
+				case 0x0004:
+					V[0xF] = (V[Y] > 0xFF - V[X]) ? 1 : 0;
+					V[X] += V[Y];
+					break;
+				case 0x0005:
+					V[0xF] = (V[Y] > V[X]) ? 1 : 0;
+					V[X] -= V[Y];
+					break;
+				case 0x0006:
+					V[0xF] = V[X] & 1;
+					V[X] >>= 1;
+					break;
+				case 0x0007:
+					V[0xF] = (V[X] > V[Y]) ? 0 : 1;
+					V[X] = V[Y] - V[X];
+					break;
+				case 0x000E:
+					V[0xF] = V[X] & 1;
+					V[X] <<= 1;
+					break;
+				default:
+					unknown_opcode(opcode);
+			}
+			PC += 2;
+			break;
+		case 0x9000:
+			switch(opcode & 0x000F)
+				{
+					case 0:
+						PC += (V[X] != V[Y]) ? 4 : 2;
+						break;
+					default:
+						unknown_opcode(opcode);
+				}
+			break;
+		case 0xA000: /*ANNN sets I to address NNN*/
+			I = opcode & 0x0FFF;
+			PC += 2;
+			break;
+		case 0xB000: /*BNNN jumps to address NNN plus V0*/
+			PC = (opcode & 0x0FFF) + V[0];
+			break;
+		case 0xC000: /*CXNN sets VX to result of & on rand no and NN*/
+			uint8_t random_no = (rand() % (256)); /*generates rand number between 0 and 255*/
+			V[X] = random_no & (opcode & 0x00FF);
+			PC += 2;
+			break;
+		case 0xD000:
+			break;
+		case 0xE000:
+			break;
+		case 0xF000:
+			break;
+		default:
+			unknown_opcode(opcode);
+	}
 }
